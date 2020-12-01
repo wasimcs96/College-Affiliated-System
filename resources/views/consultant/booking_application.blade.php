46ec193764905b822268e0e7cf87114fc6033be4
unknown <qureshisufiyan778@gmail.com>
2020-12-01 18:37:49 +0530

    made application for booking and other related stuff

--- resources/views/consultant/booking_application.blade.php
+++ resources/views/consultant/booking_application.blade.php
@@ -22,86 +22,124 @@
                     </li>
                 </ul>
             </div>
-            <div class="form-box">
-                {{-- <div class="form-title-wrap">
-                    <h3 class="title">Your Card Information</h3>
-                </div><!-- form-title-wrap --> --}}
-                <div class="form-content">
-                   <!-- end section-tab -->
-                    <div class="tab-content">
-                        <div class="tab-pane fade show active" id="credit-card" role="tabpanel" aria-labelledby="credit-card-tab">
-                            <div class="contact-form-action">
-                                <form method="post">
-                                    <div class="row">
-                                        <div class="col-lg-6 responsive-column">
-                                            <div class="input-box">
-                                                <label class="label-text">University  Name</label>
-                                                <div class="form-group">
-                                                    <span class="la la-credit-card form-icon"></span>
-                                                    <input class="form-control" type="text" name="text" placeholder="Card holder name" required>
-                                                </div>
-                                            </div>
-                                        </div><!-- end col-lg-6 -->
-                                        <div class="col-lg-6 responsive-column">
-                                            <label class="label-text">Course  Name</label>
-
-                                            <div class="input-group mb-3 FulNam" >
-                                                <div class="input-group-prepend">
-                                                    <label class="input-group-text" for="inputGroupSelect01">Course</label>
-                                                  </div>
-                                                <select class="custom-select FulNamo"  id="inputGroupSelect01">
-                                                  <option selected>Choose...</option>
-                                                  <option value="1">One</option>
-                                                  <option value="2">Two</option>
-                                                  <option value="3">Three</option>
-                                                </select>
-                                              </div>
-                                        </div>
-                                        <div class="col-md-12">
-
-                                            <div class="table-responsive" style="width: 100%;     margin-top: 36px;">
-                                                <label for="name">University  Name</label>
-                                                <table class="table table-bordered" id="dynamic_field">
-                                                    <tr>
-                                                        <td> <select id="inputState" class="form-control FulNamo" >
-                                                            <option selected>Choose...</option>
-                                                            <option>...</option>
-                                                          </select></td>
-                                                          <td> <select class="custom-select FulNamo"  id="inputGroupSelect01">
-                                                            <option selected>Choose...</option>
-                                                            <option value="1">One</option>
-                                                            <option value="2">Two</option>
-                                                            <option value="3">Three</option>
-                                                          </select></td>
-                                                        <td><button type="button" name="add" id="add" class="btn btn-primary btn-m">Add More</button></td>
-                                                    </tr>
-                                                </table>
-                                            </div>
-                                        </div>
-                                        <!-- end col-lg-6 -->
-                                      <!-- end col-lg-6 -->
-                                        <!-- end col-lg-6 -->
-                                        <!-- end col-lg-12 -->
-                                        <div class="col-lg-12">
-                                            <div class="btn-box">
-                                               <a href="#" class="btn btn-primary btn-lg">Confirm Booking</a>
-                                            </div>
-                                        </div><!-- end col-lg-12 -->
-                                    </div>
-                                </form>
-                            </div><!-- end contact-form-action -->
-                        </div><!-- end tab-pane-->
+            <div class="row clearfix">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>Form</h2>
+                        </div>
+                        <div class="body">
+                            <form id="basic-form" method="post" novalidate action="#">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" name="name" id="name" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Father Name</label>
+                                    <input type="text" class="form-control" name="father_name" id="father_name" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Mother Name</label>
+                                    <input type="text" class="form-control" name="mother_name" id="mother_name" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" name="email" id="email" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Mobile</label>
+                                    <input type="number" class="form-control" name="mobile" id="mobile" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Remarks</label>
+                                    <textarea class="form-control" rows="5" cols="30" required></textarea>
+                                </div>
 
 
-                    </div><!-- end tab-content -->
-                </div><!-- end form-content -->
+                                <div class="form-group">
+                                    <label for="selectmore">Select Universities and Courses</label>
+                                    <table class="table table-bordered" id="dynamic_field">
+                                        <tr class="dynamic-added">
+                                            <td > <select id="inputState" class="form-control FulNamo" >
+                                                <option selected>Choose University</option>
+                                                <option>RTU</option>
+                                                <option>BTU</option>
+                                                <option>CTU</option>
+                                              </select></td>
+                                              <td> <select class="form-control FulNamo"  id="inputGroupSelect01">
+                                                <option selected>Choose Course</option>
+                                                <option value="1">B.Tech</option>
+                                                <option value="2">MBA</option>
+                                                <option value="3">MBBS</option>
+                                              </select></td>
+                                            <td><button type="button" name="add" id="add" class="btn btn-primary btn-m"><i class="fa fa-plus"></i> </button></td>
+                                        </tr>
+                                    </table>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="documents">Documents</label>
+
+                                <br />
+                                <label class="control-inline fancy-checkbox">
+                            <div class="dynamic_document" id="dynamic_document">
+                                    <input type="checkbox" name="10marksheet" required data-parsley-check="[1,5]" data-parsley-errors-container="#error-checkbox3">
+                                    <span>Marksheet-10</span>
+                                </label>
+                                <label class="control-inline fancy-checkbox">
+                                    <input type="checkbox" name="aadhar">
+                                    <span>Aadhar Card</span>
+                                </label>
+                                <label class="control-inline fancy-checkbox">
+                                    <input type="checkbox" name="12marksheet">
+                                    <span>Marksheet-12</span>
+                                </label>
+
+                            </div>
+                                <button type="button" name="adddocument" id="add_document" class="btn btn-primary btn-m" data-toggle="modal" data-target="#documentModal"><i class="fa fa-plus"></i> </button>
+                                <p id="error-checkbox3"></p>
+
+
+
+
+
+                                <br>
+                                <button type="submit" class="btn btn-primary">Confirm Booking</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 
-
+<div class="modal fade" id="documentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add Document Title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form id="basic-form" method="post" novalidate action="#">
+                <div class="form-group">
+                    <label>Document Name</label>
+                    <input type="text" class="form-control" name="document_name" id="document_name" required>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+          {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> --}}
+          {{-- <button type= class="btn btn-primary">Submit</button> --}}
+<a href="javascript:void(0)" class="btn btn-primary" id="add_document2"> Add </a>
+ </div>
+      </div>
+    </div>
+  </div>
 
 
 @stop
@@ -134,14 +172,40 @@
 
 
       $('#add').click(function(){
-           i++;
-           $('#dynamic_field').append('<tr id="row'+i+'" class="dynamic-added"><td><select id="inputState" class="form-control"><option selected>Choose...</option> <option>...</option></select></td><td></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+             i++;
+           $('#dynamic_field').append('<tr  id="row'+i+'" class="dynamic-added"><td><select id="inputState" class="form-control"><option selected>Choose...</option> <option>...</option></select></td><td><select id="inputGroupSelect01" class="form-control"><option selected>Choose...</option> <option>...</option></select></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+           r=$('#dynamic_field .dynamic-added').length;
+            if(r==3){
+                $('#add').prop('disabled', true);
+            }
+
+
+
       });
 
+    //   $('#add_document').click(function(){
+    //       i++;
+    //        $('#dynamic_document').append('<label class="control-inline fancy-checkbox"><input type="checkbox" name="aadhar" class="form-control"><span><input type="text" name="document_name" class="form-control col-lg-8 "></span></label>')
+    //   });
+
+    $('#add_document2').click(function(){
+      rt=$('#document_name').val()
+    //   console.log(rt);
+$('#dynamic_document').append('<label class="control-inline fancy-checkbox"><input type="checkbox" name="12marksheet"><span>'+rt+'</span></label>')
+$('#documentModal').modal('hide')
+    });
 
       $(document).on('click', '.btn_remove', function(){
            var button_id = $(this).attr("id");
+        //    console.log(button_id);
            $('#row'+button_id+'').remove();
+           r=$('#dynamic_field .dynamic-added').length;
+            if(r<3){
+                $('#add').prop('disabled', false);
+            }
+        //    $('#add').add();
+        //    window.location.reload();
+
       });
 
 
@@ -186,5 +250,17 @@
       }
     });
 </script>
+<script src="{{ asset('assets/vendor/bootstrap-multiselect/bootstrap-multiselect.js') }}"></script>
+<script src="{{ asset('assets/vendor/parsleyjs/js/parsley.min.js') }}"></script>
 
+<script src="{{ asset('assets/bundles/mainscripts.bundle.js') }}"></script>
+<script>
+$(function() {
+    // validation needs name of the element
+    $('#food').multiselect();
+
+    // initialize after multiselect
+    $('#basic-form').parsley();
+});
+</script>
 @stop
