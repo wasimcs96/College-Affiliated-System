77ce3a2db3d8790903f31857d6eb53db9be3b8f0
unknown <qureshisufiyan778@gmail.com>
2020-12-13 16:04:01 +0530

    changes

--- app/Http/Controllers/Consultant/ConsultantProfileController.php
+++ app/Http/Controllers/Consultant/ConsultantProfileController.php
@@ -70,60 +70,39 @@ $id = Auth()->user()->id;
                  $consultant->save();
              }
 
-            //  $starttime = $request->start_time;  // your start time
-            //  $endtime = $request->end_time;  // End time
-            //  $duration = '60';  // split by 60 mins
 
-            //  $array_of_time = array ();
-            //  $start_time    = strtotime ($starttime); //change to strtotime
-            //  $end_time      = strtotime ($endtime); //change to strtotime
+            function SplitTime($StartTime, $EndTime, $Duration="30"){
+                $ReturnArray = array ();// Define output
+                $StartTime    = strtotime ($StartTime); //Get Timestamp
+                $EndTime      = strtotime ($EndTime); //Get Timestamp
 
-            //  $add_mins  = $duration * 60;
+                $AddMins  = $Duration * 60;
 
-            //  while ($start_time <= $end_time) // loop between time
-            //  {
-            //     $array_of_time[] = date ("h:i", $start_time);
-            //     $start_time += $add_mins; // to check endtime
-            //  }
-            //  dd(collect($array_of_time));
-            //  $consultant_slot = ConsultantAvailableSlots::where('consultant_id',$consultant->id)->first();
-            // //  dd($consultant->id);
-            //  if ($consultant_slot == null) {
-            //     # code...
-            //     $consultant_slotnew=ConsultantAvailableSlots::create($request->all());
-            //     $consultant_slotnew->save();
-            //     // $consultant_slotnew->user_id=$id;
-            //     // $consultant_slot->working_week_days = collect($request->weekday)->implode(',');
-            //     $consultantnew_slot->save();
-            //    }
-            // else{
-            //    $consultant_slot->fill($request->all());
-            //    $consultant_slot->save();
-            //    $consultant_slot->user_id=$id;
-            //    // $consultant->['working_week_days'] = $request->weekday;
-            //    $consultant_slot->working_week_days = collect($request->weekday)->implode(',');
-            //     $consultant_slot->save();
-            // }
-            // $consultant_slot = ConsultantAvailableSlots::where('consultant_id',$consultant->id)->first();
-            $start = new DateTime($request->start_time);
-            $end = new DateTime($request->end_time);
-            $start_time = $start->format('H:i');
-            $end_time = $end->format('H:i');
-            $duration = 60;
-            $i=0;
-            // dd($start_time);
-            while(strtotime($start_time) <= strtotime($end_time)){
-                $start = $start_time;
-                $end = date('H:i',strtotime('+'.$duration.' minutes',strtotime($start_time)));
-                $start_time = date('H:i',strtotime('+'.$duration.' minutes',strtotime($start_time)));
-                $i++;
-
-                if(strtotime($start_time) <= strtotime($end_time)){
-                    $time[$i]['start'] = $start;
-                    $time[$i]['end'] = $end;
+                while ($StartTime <= $EndTime) //Run loop
+                {
+                    // dd($StartTime);
+                    $ReturnArray[] = date ("G:i", $StartTime);
+                    $StartTime += $AddMins; //Endtime check
                 }
+                return $ReturnArray;
             }
+            $startTime = $request->start_time;
+            $endTime = $request->end_time;
+            //Calling the function
+            $data = SplitTime($startTime, $endTime, "30");
 
+            foreach($data as $ti){
+
+                $vr=strtotime($ti);
+                // $cr=;
+                ConsultantAvailableSlots::create([
+                'consultant_id'=>auth()->user()->consultant->id,
+                'week_day'=>1,
+                'start_slot_time'=>$ti,
+                'end_slot_time'=>date ("G:i", $vr+1800),
+                'status'=>1
+                ]);
+            }
 
              return redirect()->route('consultant.profile')->with('success','Profile Updated successfully');
     }
