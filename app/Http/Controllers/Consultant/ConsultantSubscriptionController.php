0cbfd72bd14b6221daa45d0ab89978ad0b9f698b
unknown <qureshisufiyan778@gmail.com>
2021-01-05 11:17:01 +0530

    changes

--- app/Http/Controllers/Consultant/ConsultantSubscriptionController.php
+++ app/Http/Controllers/Consultant/ConsultantSubscriptionController.php
@@ -11,24 +11,13 @@ class ConsultantSubscriptionController extends Controller
 {
     public function index()
     {
-
         $ua=auth()->user()->id;
-
-        // dd($subscriptions);
         $subscriptions= Order::where('user_id',$ua)->where('payment_type', 0 )->orderBy('updated_at', 'DESC')->get();
-        // dd($subscriptions);
-
         return view('consultant.subscription.subscription',compact('subscriptions'));
     }
 
     public function add()
     {
-
-
-
-        // dd($subscriptions);
-        // Order::where('user_id',$ua)->where('payment_type', 0 )->get();
-        // dd($subscription);
         $packages=Package::where('package_type', 0)->get();
         return view('consultant.subscription.subscription_add')->with('packages',$packages);
     }
