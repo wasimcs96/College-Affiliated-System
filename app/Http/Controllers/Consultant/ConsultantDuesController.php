dd4b70e52e122bc018b0d2d93c35898abd78c7d9
Saif Qureshi <saifu32444@gmail.com>
2021-01-21 11:09:27 +0530

    changes

--- app/Http/Controllers/Consultant/ConsultantDuesController.php
+++ app/Http/Controllers/Consultant/ConsultantDuesController.php
@@ -6,17 +6,28 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\ConsultantDues;
 use App\Models\Order;
+use App\Http\Controllers\PaymentController;
+
+
 class ConsultantDuesController extends Controller
 {
     public function index()
     {
         $dues = ConsultantDues::all()->first();
-        return view('consultant.dues.index')->with('dues',$dues);
+        return view('consultant.dues.index')->with('dues', $dues);
     }
 
     public function add()
     {
-        $packages=Package::where('package_type', 1)->get();
-        return view('consultant.premium.gopremium')->with('packages',$packages);
+        $packages = Package::where('package_type', 1)->get();
+        return view('consultant.premium.gopremium')->with('packages', $packages);
+    }
+    public function pay(Request $request)
+    {
+        $amount = $request->amount;
+
+        $pay = new PaymentController();
+        $res = $pay->payment($request);
+        return view('consultant.dues.pay')->with('order_detail', $res);
     }
 }
