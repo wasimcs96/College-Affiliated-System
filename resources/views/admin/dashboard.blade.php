bc43b46e4fff1949a5a2ca8e5c3ec4c657802814
Sohaib Tanwar <sohaibtanwar27@gmail.com>
2021-01-27 15:57:55 +0530

    admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,109 +18,150 @@
 
                 ?>
     <div class="col-lg-4 col-md-6">
-        <div class="card text-white bg-green">
-            <div class="card-header">Total Clients</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-success">{{$users}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
+            <div class="card">
+                <div class="body">
+                    <div class="d-flex align-items-center">
+                        <div class="icon-in-bg bg-indigo text-white rounded-circle"><i class="fa fa-user"></i></div>
+                        <div class="ml-4">
+                            <span>Total Clients</span>
+                            <h4 class="mb-0 font-weight-medium">{{$users}}</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-md-6">
-        <div class="card text-white bg-red">
-            <div class="card-header">Total University</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-danger">{{$university}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-md-6">
-        <div class="card text-white bg-blue">
-            <div class="card-header">Total Consultant</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-primary">{{$consultant}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-md-6">
-        <div class="card text-white bg-orange">
-            <?php $booking = DB::table('bookings')->count(); ?>
-            <div class="card-header">Total Bookings</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-warning">{{$booking}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
+     </div>
+     <div class="col-lg-4 col-md-6">
+        <div class="card">
+            <div class="body">
+                <div class="d-flex align-items-center">
+                    <div class="icon-in-bg bg-danger text-white rounded-circle"><i class="fa fa-university" aria-hidden="true"></i>
+                    </div>
+                    <div class="ml-4">
+                        <span>Total University</span>
+                        <h4 class="mb-0 font-weight-medium">{{$university}}</h4>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
         <div class="col-lg-4 col-md-6">
-            <div class="card text-white bg-cyan">
-                <?php $applications = DB::table('applications')->count(); ?>
-                <div class="card-header">Total Application</div>
-                <div class="card-body">
-                    <div class="pb-4 m-0 text-center h1 text-info">{{$applications}}</div>
-                    {{-- <div class="d-flex">
-                        <small class="text-muted">Previous Month</small>
-                        <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                    </div> --}}
+            <div class="card">
+                <div class="body">
+                    <div class="d-flex align-items-center">
+                        <div class="icon-in-bg bg-orange text-white rounded-circle"><i class="fa fa-briefcase" aria-hidden="true"></i>
+
+                        </div>
+                        <div class="ml-4">
+                            <span>Total Consultant</span>
+                            <h4 class="mb-0 font-weight-medium">{{$consultant}}</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    <div class="col-lg-4 col-md-6">
-        <div class="card text-white" style="background-color:teal; color:white;">
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card">
+                <?php $booking = DB::table('bookings')->count(); ?>
+                <div class="body">
+                    <div class="d-flex align-items-center">
+                        <div class="icon-in-bg bg-primary text-white rounded-circle"><i class="fa fa-sticky-note" aria-hidden="true"></i>
+                        </div>
+                        <div class="ml-4">
+                            <span>Total Bookings</span>
+                            <h4 class="mb-0 font-weight-medium">{{$booking}}</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+     </div>
+
+     <div class="col-lg-4 col-md-6">
+        <div class="card">
+            <?php $applications = DB::table('applications')->count(); ?>
+
+            <div class="body">
+                <div class="d-flex align-items-center">
+                    <div class="icon-in-bg bg-success text-white rounded-circle"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </div>
+                    <div class="ml-4">
+                        <span>Total Application</span>
+                        <h4 class="mb-0 font-weight-medium">{{$applications}}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+
+     <div class="col-lg-4 col-md-6">
+        <div class="card">
             <?php $fly = DB::table('university_consultant_clients')->count(); ?>
-            <div class="card-header">Ready to Fly Clients</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-info">{{$fly}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
+
+            <div class="body">
+                <div class="d-flex align-items-center">
+                    <div class="icon-in-bg bg-blue text-white rounded-circle"><i class="fa fa-users" aria-hidden="true"></i></div>
+                    <div class="ml-4">
+                        <span>Ready to Fly Clients</span>
+                        <h4 class="mb-0 font-weight-medium">{{$fly}}</h4>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+ </div>
 
-    <div class="col-lg-4 col-md-6">
-        <div class="card text-white " style="background-color:slateblue; color:white;">
+ <div class="col-lg-4 col-md-6">
+    <div class="card">
+        <div class="body">
             <?php $pr = DB::table('bookings')->where('booking_for',1)->count(); ?>
-            <div class="card-header">PR Requests</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-info">{{$pr}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
-            </div>
-        </div>
-    </div>
 
-    <div class="col-lg-4 col-md-6">
-        <div class="card text-white " style="background-color: palegreen; color:white;">
-            <?php $revenue = App\Models\Order::all()->sum('amount');?>
-            <div class="card-header">Total Revenue</div>
-            <div class="card-body">
-                <div class="pb-4 m-0 text-center h1 text-info">{{$revenue}}</div>
-                {{-- <div class="d-flex">
-                    <small class="text-muted">Previous Month</small>
-                    <div class="ml-auto"><i class="fa fa-caret-up text-success"></i>4.00%</div>
-                </div> --}}
+            <div class="d-flex align-items-center">
+                <div class="icon-in-bg bg-green text-white rounded-circle"><i class="fa fa-plane" aria-hidden="true"></i>
+                </div>
+                <div class="ml-4">
+                    <span>PR Requests</span>
+                    <h4 class="mb-0 font-weight-medium">{{$pr}}</h4>
+                </div>
             </div>
         </div>
     </div>
+</div>
+<div class="col-lg-4 col-md-6">
+    <div class="card">
+        <div class="body">
+
+            <?php $mytime=Carbon\Carbon::now()->format('Y-m-d');?>
+            <td>
+            <?php $ad = App\Models\Advertisement::where('expire_date','>',$mytime)->count()?>
+
+            <div class="d-flex align-items-center">
+                <div class="icon-in-bg bg-cyan text-white rounded-circle"><i class="fa fa-adn" aria-hidden="true"></i>
+                </div>
+                <div class="ml-4">
+                    <span>Active Advertisement</span>
+                    <h4 class="mb-0 font-weight-medium">{{$ad}}</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6">
+    <div class="card">
+        <div class="body">
+            <?php $revenue = App\Models\Order::all()->sum('amount');?>
+
+            <div class="d-flex align-items-center">
+                <div class="icon-in-bg bg-cyan text-white rounded-circle"><i class="fa fa-money" aria-hidden="true"></i>
+                </div>
+                <div class="ml-4">
+                    <span>Total Revenue</span>
+                    <h4 class="mb-0 font-weight-medium"><i class="fa fa-inr" aria-hidden="true"></i>{!!"&nbsp"!!}{{$revenue}}</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </div>
 
 
