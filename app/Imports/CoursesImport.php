f4c80dca558534a5bf670bf2d24250594c2c6362
unknown <qureshisufiyan778@gmail.com>
2021-02-16 10:22:02 +0530

    changes

--- app/Imports/CoursesImport.php
+++ app/Imports/CoursesImport.php
@@ -5,10 +5,17 @@ namespace App\Imports;
 use App\Models\UniversityCourse;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Validators\Failure;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Rule;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Carbon;
 
-class CoursesImport implements ToModel, WithStartRow
+class CoursesImport implements ToModel, WithStartRow, WithValidation, WithHeadingRow
 {
+    use Importable;
   public $category;
 
 
@@ -22,6 +29,24 @@ class CoursesImport implements ToModel, WithStartRow
     {
         return 2;
     }
+
+    // public function rules(): array
+    // {
+    //     return [
+
+
+    //          // Can also use callback validation rules
+    //          '0' => function($attribute, $value, $onFailure) {
+    //               if ($value == '') {
+    //                    $onFailure('row 0 is blank');
+    //               }
+    //           }
+
+    //     ];
+    // }
+
+
+
     /**
     * @param array $row
     *
@@ -29,6 +54,13 @@ class CoursesImport implements ToModel, WithStartRow
     */
     public function model(array $row)
     {
+        // if($row[0]=='' || $row[1]=='' || $row[2]=='' || $row[3]=='')
+        // {
+        //     if($row[3]=='')
+        //     {
+        //         dd($row[0]);
+        //     }
+        // }
         // $course = UniversityCourse::where('title',$row[1])->where('user_id',auth()->user()->id)->get()->first();
         // $adminCourse= Course::where('id',$row[0])->get()->first();
         $start_date = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[3]))->format('Y-m-d');
@@ -48,9 +80,60 @@ class CoursesImport implements ToModel, WithStartRow
             'start_date' => $start_date,
             'end_date' => $end_date,
         ]);
+
+
+
+    //     foreach ($import->failures() as $failure) {
+    //         // $failure->row(); // row that went wrong
+    //         // $failure->attribute(); // either heading key (if using heading row concern) or column index
+    //         // $failure->errors(); // Actual error messages from Laravel validator
+    //         $failure->values(); // The values of the row that has failed.
+    //    }
         // }
         // if($adminCourse)
         // {
         // }
+
     }
+
+    // public function rules(): array
+    // {
+    //     return [
+    //         '0' => 'required',
+    //         '*.0' => 'required',
+    //         '1' => 'required',
+    //         '*.1' => 'required',
+    //         '2' => 'required',
+    //         '*.2' => 'required',
+    //         '3' => 'required',
+    //         '*.3' => 'required',
+    //         '4' => 'required',
+    //         '*.4' => 'required',
+    //     ];
+    // }
+
+    // /**
+    //  * @param Failure ...$failures
+    //  */
+    // public function onFailure(Failure ...$failures)
+    // {
+    //     Log::stack(['import-failure-logs'])->info(json_encode($failures));
+    // }
+
+    // public function rules(): array
+    // {
+    //     return [
+    //         '0' => 'required',
+    //         '*.0' => 'required',
+    //         '1' => 'required',
+    //         '*.1' => 'required',
+    //         '2' => 'required',
+    //         '*.2' => 'required',
+    //         '3' => 'required',
+    //         '*.3' => 'required',
+    //         '4' => 'required',
+    //         '*.4' => 'required',
+    //         'maximum' => 'gte:*.minimum',
+    //     ];
+    // }
 }
